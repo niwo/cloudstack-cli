32308a69947a13442c55c01d75066ba912612517
niwo <nik.wolfgramm@gmail.com>
2014-02-22 21:22:38 +0100

    add arg completion

--- completions/cs.bash
+++ completions/cs.bash
@@ -8,6 +8,14 @@ _cs() {
     if [ $(echo ${words[@]} | wc -w) -eq 2 ]; then
       COMPREPLY=( $(compgen -W "$(${words[@]} help | grep cs | cut -d ' ' -f5)" -- "$word") )
     fi
+    if [ $(echo ${words[@]} | wc -w) -ge 3 ]; then
+      local cp1=$(echo ${words[@]} | cut -d ' ' -f1-2)
+      local cp2=$(echo ${words[@]} | cut -d ' ' -f3)
+      if [ "$cp2" != "help" ]; then
+        local cp3=$($cp1 help $cp2)
+        COMPREPLY=( $(compgen -W "$(echo $cp3 | awk 'NR>1{print $1}' RS=[ FS='\=')" -- "$word") )
+      fi
+    fi
   fi
 }
 
