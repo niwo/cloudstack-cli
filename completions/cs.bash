fce33aa6fd8fbb6f794dd40893bf54f5354aaa9f
niwo <nik.wolfgramm@gmail.com>
2015-02-02 14:43:34 +0100

    updated bash autocompletion

--- completions/cs.bash
+++ completions/cs.bash
@@ -3,7 +3,7 @@
 # cloudstack-cli
 # https://github.com/niwo/cloudstack-cli
 #
-# Copyright (c) 2014 Nik Wolfgramm
+# Copyright (c) 2015 Nik Wolfgramm
 # Licensed under the MIT license.
 # https://raw.github.com/niwo/cloudstack-cli/master/LICENSE.txt
 
@@ -12,14 +12,14 @@
 # To enable bash <tab> completion for cloudstack-cli, add the following line (minus the
 # leading #, which is the bash comment character) to your ~/.bash_profile file (use ~/.bashrc on Ubuntu):
 #
-# eval "$(cs completion --shell=bash)"
+# eval "$(cloudstack-cli completion --shell=bash)"
 
-_cs() {
+_cloudstack_cli() {
   COMPREPLY=()
   local word="${COMP_WORDS[COMP_CWORD]}"
   # list available base commands
   if [ "$COMP_CWORD" -eq 1 ]; then
-    COMPREPLY=( $(compgen -W "$(cs help | grep cs | cut -d ' ' -f4)" -- "$word") )
+    COMPREPLY=( $(compgen -W "$(cloudstack-cli help | grep cloudstack-cli | cut -d ' ' -f4)" -- "$word") )
   else
     local words=("${COMP_WORDS[@]}")
     # ignore commands which contain 'help'
@@ -28,7 +28,7 @@ _cs() {
     # search for subcommand
     elif [[ "$word" != -* ]] && [ "$COMP_CWORD" -eq 2 ]; then
       local cp1=$(echo ${words[@]} | cut -d ' ' -f1-2)
-      COMPREPLY=( $(compgen -W "$($cp1 help | grep cs | cut -d ' ' -f5)" -- "$word") )
+      COMPREPLY=( $(compgen -W "$($cp1 help | grep cloudstack-cli | cut -d ' ' -f5)" -- "$word") )
     # list options for the subcommand
     elif [[ "$word" =~ -* ]] && [ "$COMP_CWORD" -gt 2 ]; then
       local cp1=$(echo ${words[@]} | cut -d ' ' -f1-2)
@@ -39,4 +39,5 @@ _cs() {
   fi
 }
 
-complete -F _cs cs
+complete -F _cloudstack_cli cs
+complete -F _cloudstack_cli cloudstack-cli
