a4a2b73cb92ec0573b06a53420b78dcad8c2e361
niwo <nik.wolfgramm@gmail.com>
2015-02-09 18:57:35 +0100

    add update and refresh functions for resource_limits

--- lib/cloudstack-cli/commands/account.rb
+++ lib/cloudstack-cli/commands/account.rb
@@ -22,15 +22,20 @@ class Account < CloudstackCli::Base
     end
   end
 
-  desc 'list [NAME]', 'list accounts (by name)'
-  def list(name = nil)
-    accounts = client.list_accounts(name: name)
+  desc 'list', 'list accounts'
+  def list
+    accounts = client.list_accounts
     if accounts.size < 1
       puts "No accounts found."
     else
-      table = [["Name", "Type", "Domain"]]
+      table = [%w(Name Type Domain State)]
       accounts.each do |account|
-        table << [account['name'], TYPES[account['accounttype']], account['domain']]
+        table << [
+          account['name'],
+          TYPES[account['accounttype']],
+          account['domain'],
+          account['state']
+        ]
       end
       print_table table
       say "Total number of accounts: #{accounts.size}"
--- lib/cloudstack-cli/commands/resource_limit.rb
+++ lib/cloudstack-cli/commands/resource_limit.rb
@@ -36,6 +36,55 @@ class ResourceLimit < CloudstackCli::Base
     print_table table
   end
 
+  desc "refresh", "refresh resource counts"
+  option :domain, desc: "refresh resource for a specified domain"
+  option :account, desc: "refresh resource for a specified account"
+  option :project, desc: "refresh resource for a specified project"
+  option :type, desc: "specify type, see types for a list of types"
+  def refresh
+    set_domain_id if options[:domain]
+    options[:resource_type] = options[:type] if options[:type]
+
+    unless ['domain_id', 'account', 'project'].any? {|k| options.key?(k)}
+      say "Error: Please provide domain, account or project.", :red
+      exit 1
+    end
+
+    if resource_count = client.update_resource_count(options)
+      say "Sucessfully refreshed resource limits.", :green
+    else
+      say "Error refreshing resource limits.", :red
+      exit 1
+    end
+  end
+
+  desc "update", "update resource counts"
+  option :domain, desc: "update resource for a specified domain"
+  option :account, desc: "update resource for a specified account"
+  option :project, desc: "update resource for a specified project"
+  option :type,
+    desc: "specify type, see types for a list of types",
+    required: true
+  option :max,
+    desc: "Maximum resource limit.",
+    required: true
+  def update
+    set_domain_id if options[:domain]
+    options[:resource_type] = options[:type]
+
+    unless ['domain_id', 'account', 'project'].any? {|k| options.key?(k)}
+      say "Error: Please provide domain, account or project.", :red
+      exit 1
+    end
+
+    if resource_count = client.update_resource_limit(options)
+      say "Sucessfully updated resource limits.", :green
+    else
+      say "Error updating resource limits.", :red
+      exit 1
+    end
+  end
+
   desc "types", "show resource types"
   def types
     table = [['type', 'name']]
@@ -48,12 +97,22 @@ class ResourceLimit < CloudstackCli::Base
   no_commands do
 
     def resource_to_s(limit, entity)
+      return '-1 (unlimited)' if limit['max'] == -1
       value = RESOURCE_TYPES[limit['resourcetype']][:divider] ?
       (limit[entity] / RESOURCE_TYPES[limit['resourcetype']][:divider]).round(1) :
       limit[entity]
       RESOURCE_TYPES[limit['resourcetype']][:unit] ?
       "#{value} #{RESOURCE_TYPES[limit['resourcetype']][:unit]}" :
       value.to_s
+    end
+
+    def set_domain_id
+      domains = client.list_domains(options[:domain])
+      if domains.size < 1
+        say "Domain #{options[:domain]} not found.", :red
+      else
+        options[:domain_id] = domains.first['id']
+      end
     end
 
   end
