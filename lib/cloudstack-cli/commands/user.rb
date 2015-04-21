f044de1ddb7e8f6b4b4158194092ce95ce3a5186
niwo <nik.wolfgramm@gmail.com>
2015-04-22 00:27:57 +0200

    migrated user command

--- lib/cloudstack-cli/commands/user.rb
+++ lib/cloudstack-cli/commands/user.rb
@@ -1,15 +1,10 @@
 class User < CloudstackCli::Base
 
-  USER_TYPES = {
-    0 => 'user',
-    1 => 'domain-admin',
-    2 => 'admin'
-  }
-
   desc 'list', 'list users'
   option :listall
   option :account
   def list
+    resolve_account
     users = client.list_users(options)
     if users.size < 1
       say "No users found."
@@ -17,7 +12,7 @@ class User < CloudstackCli::Base
       table = [["Account", "Type", "Name", "Email", "State", "Domain"]]
       users.each do |user|
         table << [
-          user['account'], USER_TYPES[user['accounttype']], "#{user['firstname']} #{user['lastname']}",
+          user['account'], Account::TYPES[user['accounttype']], "#{user['firstname']} #{user['lastname']}",
           user['email'], user['state'], user['domain']
         ]
       end
@@ -25,5 +20,5 @@ class User < CloudstackCli::Base
       say "Total number of users: #{users.size}"
     end
   end
-  
+
 end
