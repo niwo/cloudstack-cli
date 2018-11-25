1d0a720eca77024da8a8b76344857baf19e3e1a6
niwo <nik.wolfgramm@gmail.com>
2018-11-25 01:29:43 +0100

    updated gems, rescue template date exceptions

--- lib/cloudstack-cli/version.rb
+++ lib/cloudstack-cli/version.rb
@@ -1,3 +1,3 @@
 module CloudstackCli
-  VERSION = "1.6.3"
+  VERSION = "1.6.4"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,9 @@ require "minitest/spec"
 require "minitest/autorun"
 require "minitest/pride"
 
-# make the config file setup awailable to all specs
+# make the config file setup available to all specs
 CONFIG = "--config-file=#{File.expand_path('cloudstack.yml', File.dirname(__FILE__))}"
 ZONE = "Sandbox-simulator"
-TEMPLATE = "CentOS 5.3(64-bit) no GUI (Simulator)"
+TEMPLATE = "CentOS 5.6 (64-bit) no GUI (Simulator)"
 OFFERING_S = "Small Instance"
 OFFERING_M = "Medium Instance"
